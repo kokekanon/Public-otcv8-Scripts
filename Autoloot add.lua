14075b1be0527b1c359d0599e51e241fdd0e3d32
kokekanon <114332266+kokekanon@users.noreply.github.com>
2023-06-11 02:28:26 -0400

    autolook add on look
    test

---
+++ Autoloot add.lua
@@ -0,0 +1,10 @@
+local doAutoLootLook = macro(5000, "Auto Loot ADD on Look", function() end)
+onTextMessage(function(mode, text)
+    if mode == 20 and text:find("You see") and doAutoLootLook:isOn() then
+        local regex = [[(?:an|a)([a-z A-Z]*).]]
+        local data = regexMatch(text, regex)[1]
+        if data and data[2] then
+            say('!autoloot add, ' ..data[2]:trim())
+        end
+    end
+end)
