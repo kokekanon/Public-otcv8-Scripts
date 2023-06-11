39b76a824e08eff8055daa270e20c1a3d0722833
kokekanon <114332266+kokekanon@users.noreply.github.com>
2023-06-11 13:44:23 -0400

    Update Skills_Hud.lua

--- Skills_Hud.lua
+++ Skills_Hud.lua
@@ -1,3 +1,5 @@
+
+
 local panelName = "tcSkills"
 local tcSkillPanel = setupUI([[
 OutlineLabel < Label
@@ -35,60 +37,19 @@ local skills = {
   { name = "Life Leech Chance", data = {}, id = 9, enabled = false },
   { name = "Life Leech Amount", data = {}, id = 10, enabled = false },
   { name = "Mana Leech Chance", data = {}, id = 11, enabled = false },
-  { name = "Mana Leech Amount", data = {}, id = 12, enabled = false },
+  { name = "Mana Leech Amount", enabled = true , data = {}, id = 12, enabled = false },
   { name = "Level",  data = {}, id = 13, enabled = true, color = '#aba71e', highlight = 'orange' },
   { name = "Magic Level", data = {}, id = 14, enabled = true , color = '#4fc3f7', highlight = 'green'},
   { name = "Stamina", data = {}, id = 15, enabled = true },
 }
 
-local firstSkill, lastSkill = 1, 13 -- unused
+local firstSkill, lastSkill = 1, 13
 local levelSkill = 14
 local magicSkill = 15
 local stamSkill = 16
 
-function checkLevel(level, levelPercent)
-    local skill = skills[levelSkill]
-    if skill and skill.enabled then
-      local label = tcSkillPanel[levelSkill] or UI.createWidget("OutlineLabel", tcSkillPanel)
-      label:setId(levelSkill)
-      label:setColoredText({
-        '~ '..skill.name..': ', (skill.color or 'white'),
-        level, (skill.highlight or 'green'),
-        ' ('.. levelPercent.. '%)', (skill.color or 'white'),
-      })
-    end
-end
-
-function checkMagicLevel( magicLevel, magicLevelPercent)
-    local skill = skills[magicSkill]
-    if skill and skill.enabled then
-      local label = tcSkillPanel[magicSkill] or UI.createWidget("OutlineLabel", tcSkillPanel)
-      label:setId(magicSkill)
-      label:setColoredText({
-        '~ '..skill.name..': ', (skill.color or 'white'),
-        magicLevel,(skill.highlight or 'green'),
-        ' ('..magicLevelPercent.. '%)', (skill.color or 'white'),
-      })
-    end
-end
-
-function checkSkill(id, level, levelPercent)
-    local skill = skills[id]
-    if skill and skill.enabled and skill.id then
-      local label = tcSkillPanel[skill.id] or UI.createWidget("OutlineLabel", tcSkillPanel)
-        label:setId(skill.id)
-
-      local t = {
-        '~ '..skill.name..': ', (skill.color or 'white'),
-        level, (skill.highlight or 'green'),
-        ' ('..levelPercent.. '%)', (skill.color or 'white'),
-      }
-      label:setColoredText(t)
-    end
-end
-
-function calcStamina(stamina)
-  local stam = stamina
+function calcStamina()
+  local stam = stamina()
   local hours = math.floor(stam / 60)
   local minutes = stam % 60
   if minutes < 10 then
@@ -98,44 +59,76 @@ function calcStamina(stamina)
   return hours.. ':'.. minutes, ' ('..percent..'%)'
 end
 
-function checkStamina(stamina)
-    local skill = skills[stamSkill]
-    if skill and skill.enabled then
-      local label = tcSkillPanel[stamSkill] or UI.createWidget("OutlineLabel", tcSkillPanel)
-      label:setId(stamSkill)
-      local sta, per = calcStamina(stamina)
-      label:setColoredText({
-        '~ '..skill.name..': ', (skill.color or 'white'),
-        sta,(skill.highlight or 'green'),
-        per, (skill.color or 'white'),
-      })
-    end
+function checkStamina()
+  local skill = skills[stamSkill]
+  if skill and skill.enabled then
+    local label = tcSkillPanel[stamSkill] or UI.createWidget("OutlineLabel", tcSkillPanel)
+    label:setId(stamSkill)
+    local sta, per = calcStamina()
+    label:setColoredText({
+      '~ '..skill.name..': ', (skill.color or 'white'),
+      sta,(skill.highlight or 'green'),
+      per, (skill.color or 'white'),
+    })
+  end
 end
 
-onStaminaChange(function(stamina, oldStamina)
-    checkStamina(stamina)
-end)
+function checkMagicLevel()
+  local skill = skills[magicSkill]
+  if skill.enabled then
+    local label = tcSkillPanel[magicSkill] or UI.createWidget("OutlineLabel", tcSkillPanel)
+    label:setId(magicSkill)
 
-onMagicLevelChange(function( magicLevel, magicLevelPercent, oldMagicLevel, oldMagicLevelPercent)
-    checkMagicLevel(magicLevel, magicLevelPercent)
-end)
-
-onSkillChange(function(skill, level, levelPercent, oldLevel, oldLevelPercent)
-    checkSkill(skill+1, level, levelPercent)
-    warn(skill)
-end)
-
-onLevelChange(function(level, levelPercent, oldLevel, oldLevelPercent)
-    checkLevel(level, levelPercent)
-end)
-
-function initCheck()
-  checkLevel(level(),player:getLevelPercent())
-  checkMagicLevel(player:getMagicLevel(), player:getMagicLevelPercent())
-  for i = 0, 12 do
-    checkSkill(i+1, player:getSkillLevel(i), player:getSkillLevelPercent(i))
+    label:setColoredText({
+      '~ '..skill.name..': ', (skill.color or 'white'),
+      player:getMagicLevel(),(skill.highlight or 'green'),
+      ' ('..player:getMagicLevelPercent().. '%)', (skill.color or 'white'),
+    })
   end
-  checkStamina(stamina())
+end
+
+function checkSkill(num)
+  local skill = skills[num]
+  if skill and skill.enabled and skill.id then
+    local label = tcSkillPanel[skill.id] or UI.createWidget("OutlineLabel", tcSkillPanel)
+      label:setId(skill.id)
+
+    local t = {
+      '~ '..skill.name..': ', (skill.color or 'white'),
+      player:getSkillLevel(skill.id), (skill.highlight or 'green'),
+      ' ('..player:getSkillLevelPercent(skill.id).. '%)', (skill.color or 'white'),
+    }
+    label:setColoredText(t)
+  end
+end
+
+
+function checkLevel()
+  local skill = skills[levelSkill]
+  if skill.enabled then
+    local label = tcSkillPanel[levelSkill] or UI.createWidget("OutlineLabel", tcSkillPanel)
+    label:setId(levelSkill)
+
+    label:setColoredText({
+      '~ '..skill.name..': ', (skill.color or 'white'),
+      level(), (skill.highlight or 'green'),
+      ' ('.. player:getLevelPercent().. '%)', (skill.color or 'white'),
+    })
+  end
+end
+
+function checkAll()
+  checkLevel()
+  checkMagicLevel()
+  for i = firstSkill, lastSkill do
+    checkSkill(i, true)
+  end
+  checkStamina()
   tcSkillPanel:setHeight((tcSkillPanel:getChildCount()*13))
 end
-initCheck()
+
+local checkSkills = macro(2000, 'Check Skills', function()
+  checkAll()
+end)
+
+checkAll()
