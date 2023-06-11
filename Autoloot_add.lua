305a2456f28b416d842c5aa1b86829f723ec208d
kokekanon <114332266+kokekanon@users.noreply.github.com>
2023-06-11 03:16:44 -0400

    Update Autoloot_add.lua

--- Autoloot_add.lua
+++ Autoloot_add.lua
@@ -8,3 +8,15 @@ onTextMessage(function(mode, text)
         end
     end
 end)
+
+
+local doAutoLootLook = macro(5000, "Auto Loot REMOVE on Look", function() end)
+onTextMessage(function(mode, text)
+    if mode == 20 and text:find("You see") and doAutoLootLook:isOn() then
+        local regex = [[(?:an|a)([a-z A-Z]*).]]
+        local data = regexMatch(text, regex)[1]
+        if data and data[2] then
+            say('!autoloot remove, ' ..data[2]:trim())
+        end
+    end
+end)
