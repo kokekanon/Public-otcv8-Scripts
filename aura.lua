2ef91105deae2e2594bb7ebed981ac3187c3a48e
kokekanon <114332266+kokekanon@users.noreply.github.com>
2023-06-11 20:33:04 -0400

    Create aura.lua

---
+++ aura.lua
@@ -0,0 +1,15 @@
+local aura = 893
+
+local getOutfits = function()
+    local pOutfit = player:getOutfit()
+    player:setOutfit({type = pOutfit.type, wings = 0, aura = storage.aura})
+end
+
+singlehotkey("Ctrl+x", "Get Outfits", function() getOutfits() end)
+
+UI.Label("Aura:")
+UI.TextEdit(tostring(storage.aura or 893), function(widget, text)
+    storage.aura = tonumber(text)
+end)
+
+UI.Separator()
