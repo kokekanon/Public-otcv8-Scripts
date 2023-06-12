6d25b668cac28833ba3ac5e4410efc0943de0e37
kokekanon <114332266+kokekanon@users.noreply.github.com>
2023-06-12 17:40:28 -0400

    Update senzu.lua

--- senzu.lua
+++ senzu.lua
@@ -14,3 +14,21 @@ if type(storage.healing) ~= "table" then
     storage.healing = newParams
     healingmacro.setOn(storage.healing.on)
   end)
+
+
+  if type(storage.healing) ~= "table" then
+    storage.healing = {on=false, title="Heal", item=11862, min=50, max=50}
+  end
+  local healingmacro2 = macro(200, function()
+    local hppc2 = hppercent()
+    local mppc2 = manapercent()
+  
+    if hppc2 <= storage.healing.min or mppc2 <= storage.healing.max then
+      use(storage.healing.item)
+    end
+  end)
+  healingmacro2.setOn(storage.healing.on)
+  UI.DualScrollItemPanel(storage.healing, function(widget, newParams) 
+    storage.healing = newParams
+    healingmacro2.setOn(storage.healing.on)
+  end)
