a52463348321d149d8725597eb695a80a8dc290a
kokekanon <114332266+kokekanon@users.noreply.github.com>
2023-06-15 00:53:09 -0400

    Update Face_Target_Direction.lua

--- Face_Target_Direction.lua
+++ Face_Target_Direction.lua
@@ -2,23 +2,25 @@ macro(200, "Face Target", function()
     --Join Discord server for free scripts
     --https://discord.gg/RkQ9nyPMBH
     local target = g_game.getAttackingCreature()
-    if not target then return end
-    local xDiff = target:getPosition().x > posx()
-    local yDiff = target:getPosition().y > posy()
-    local isXBigger = math.abs(target:getPosition().x - posx()) > math.abs(target:getPosition().y - posy())
+    if target then
+        local xDiff = target:getPosition().x > posx()
+        local yDiff = target:getPosition().y > posy()
+        local isXBigger = math.abs(target:getPosition().x - posx()) > math.abs(target:getPosition().y - posy())
+    
+        local dir = player:getDirection()
+        if xDiff and isXBigger then  
+            if dir ~= 1 then turn(1) end
+            return
+        elseif not xDiff and isXBigger then 
+            if dir ~= 3 then turn(3) end
+            return
+        elseif yDiff and not isXBigger then  
+            if dir ~= 2 then turn(2) end
+            return
+        elseif not yDiff and not isXBigger  then 
+            if dir ~= 0 then turn(0) end
+            return
+        end
 
-    local dir = player:getDirection()
-    if xDiff and isXBigger then  
-        if dir ~= 1 then turn(1) end
-        return
-    elseif not xDiff and isXBigger then 
-        if dir ~= 3 then turn(3) end
-        return
-    elseif yDiff and not isXBigger then  
-        if dir ~= 2 then turn(2) end
-        return
-    elseif not yDiff and not isXBigger  then 
-        if dir ~= 0 then turn(0) end
-        return
-    end
+    end    
 end)
