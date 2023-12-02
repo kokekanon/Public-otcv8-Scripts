b7d496807ceb57248edbf122396f00f36385b1f2
kokekanon <114332266+kokekanon@users.noreply.github.com>
2023-12-01 23:10:52 -0300

    Update Face_Target_Direction.lua

--- Face_Target_Direction.lua
+++ Face_Target_Direction.lua
@@ -1,30 +1,34 @@
-macro(200, "Face Target", function()
 
-    --Join Discord server for free scripts
-    --https://discord.gg/RkQ9nyPMBH
-    local target = g_game.getAttackingCreature()
-    if target then
-        local xDiff = target:getPosition().x > posx()
+macro(200, "Turn to Target", function ()
+	if not g_game.isAttacking() then
+		return
+	end
 
+	local tt = g_game.getAttackingCreature()
+	local tx = tt:getPosition().x
+	local ty = tt:getPosition().y
+	local dir = player:getDirection()
+	local tdx = math.abs(tx - pos().x)
+	local tdy = math.abs(ty - pos().y)
 
-        local yDiff = target:getPosition().y > posy()
+	if tdy >= 2 and tdx >= 2 or tdx > 7 or tdy > 7 then
+		return
+	end
 
-        local isXBigger = math.abs(target:getPosition().x - posx()) > math.abs(target:getPosition().y - posy())
-        local dir = player:getDirection()
-
-        if xDiff and isXBigger then  
-            if dir ~= 1 then turn(1) end
-            return
-        elseif not xDiff and isXBigger then 
-            if dir ~= 3 then turn(3) end
-            return
-        elseif yDiff and not isXBigger then  
-            if dir ~= 2 then turn(2) end
-            return
-        elseif not yDiff and not isXBigger  then 
-            if dir ~= 0 then turn(0) end
-            return
-        end
-
-    end    
+	if tdx <= tdy then
+		if pos().y < ty then
+			if dir ~= 2 then
+				return turn(2)
+			end
+		elseif dir ~= 0 then
+			return turn(0)
+		end
+	elseif pos().x < tx then
+		if dir ~= 1 then
+			return turn(1)
+		end
+	elseif dir ~= 3 then
+		return turn(3)
+	end
 end)
+UI.Separator()
